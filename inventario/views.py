6d52d8519df88bd8def15bb57eedc0c50ae70897
Angel Medina <angelmedinaacori2015@gmail.com>
2025-11-27 13:34:24 -0300

    Avance atómico: modelos de usuarios e inventario, templates y vistas. No se implementó lógica específica de usuarios aún.

--- inventario/views.py
+++ inventario/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Producto, Categoria
 
 # Create your views here.
+def home(request):
+    titulo = "Inventario de Productos"
+    productos = Producto.objects.all()
+    categorias = Categoria.objects.all()
+    return render(request, 'inventario_home.html', {
+        'titulo': titulo,
+        'productos': productos,
+        'categorias': categorias
+        })
